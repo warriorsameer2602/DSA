3d2e3b1d528625f1abce2263a4459dd951ac8cab
Sameer Gupta <warrior20032003@gmail.com>
2026-01-10 13:48:24 +0530

    New line in stack1.java added at 6

--- Stack/stack1.java
+++ Stack/stack1.java
@@ -3,5 +3,6 @@ package Stack;
 public class stack1 {
     public static void main(String[] args) {
         System.out.println("Introduction to Stack");
+        System.out.println("Hello");
     }
 }
