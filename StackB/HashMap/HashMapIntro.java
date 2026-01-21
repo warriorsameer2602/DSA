5caa8d1ace5b85852ff159fead1922e9f1370a0a
warriorsameer2602 <warrior20032003@gmail.com>
2026-01-21 23:54:29 +0530

    HashMap added

---
+++ StackB/HashMap/HashMapIntro.java
@@ -0,0 +1,40 @@
+package StackB.HashMap;
+import java.util.*;
+public class HashMapIntro {
+    public static void main(String[] args) {
+        HashMap<String,Integer> hm = new HashMap<>();
+
+        //insert - O(1)
+        hm.put("India", 145);
+        hm.put("China",144);
+        hm.put("US",32);
+
+        System.out.println(hm);
+
+        //Get - O(1)
+        int population = hm.get("India");
+        System.out.println(population);
+        System.out.println(hm.get("Indonesia"));
+
+        //ConatinsKey - O(1)
+
+        System.out.println(hm.containsKey("India"));//true
+         System.out.println(hm.containsKey("Indonesia"));//false
+        
+
+         //remove-O(1)
+         System.out.println(hm.remove("China"));
+         System.out.println(hm);
+
+         //size
+         System.out.println(hm.size());
+
+         //isEmpty
+         System.out.println(hm.isEmpty());
+
+         //make empty the hash map
+
+         hm.clear();
+         System.out.println(hm);
+    }
+}
---
+++ StackB/HashMap/InterationOnHashMap.java
@@ -0,0 +1,22 @@
+package StackB.HashMap;
+import java.util.*;
+public class InterationOnHashMap {
+    public static void main(String[] args) {
+        HashMap<String,Integer> hm = new HashMap<>();
+        hm.put("India", 145);
+        hm.put("China", 144);
+        hm.put("US",32 );
+        hm.put("Indonesia", 11);
+        hm.put("Nepal", 5);
+
+        //Interate using set
+
+        Set<String> keys = hm.keySet();
+        System.out.println(keys);
+
+        for(String k : keys){
+            System.out.println("key=" + k + ",value=" + hm.get(k));
+        }
+
+    }
+}
